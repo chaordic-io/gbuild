10c701e35ee6251493e073bdb4064fbb28368c97
Wille Faler <wille.faler@gmail.com>
2021-04-01 23:21:59 +0200

    remove output

--- internal/config/config.go
+++ internal/config/config.go
@@ -88,7 +88,6 @@ func validate(c *Config, log common.Log) error {
 		var planTargets []string
 		planNames = append(planNames, plan.Name)
 		for _, target := range plan.Targets {
-			fmt.Println(target)
 			if !containsString(target, targetNames) {
 				return fmt.Errorf("the target %v in the execution plan %v is not defined among the targets", target, plan.Name)
 			}
