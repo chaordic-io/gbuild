07ad77ac9420d42950583b5b98ef030221f73685
reedkihaddi <saurabhbraryo@gmail.com>
2021-04-18 00:12:55 +0530

    added example ldlflags

--- internal/version.go
+++ internal/version.go
@@ -10,17 +10,23 @@ const ApplicationName = "gbuild"
 
 // Default build-time variables
 // Values are overriden via ldflags
+// Example:
+// export GIT_TAG=$(git describe --tags $(git rev-list --tags --max-count=1)) && \
+//   go build -o gbuild \
+//   -ldflags "-X 'github.com/chaordic-io/gbuild/internal.BuildDate=$(date)'\
+// 			-X 'github.com/chaordic-io/gbuild/internal.Version=$GIT_TAG'"
+
 var (
-	version   = "development"
-	buildDate = notProvided
-	platform  = fmt.Sprintf("%s/%s", runtime.GOOS, runtime.GOARCH)
-	goVersion = runtime.Version()
+	Version   = "development"
+	BuildDate = notProvided
+	Platform  = fmt.Sprintf("%s/%s", runtime.GOOS, runtime.GOARCH)
+	GoVersion = runtime.Version()
 )
 
-func PrintVersion() {
+func PrintVersionInfo() {
 	fmt.Println("Application:    ", ApplicationName)
-	fmt.Println("Version:        ", version)
-	fmt.Println("BuildDate:      ", buildDate)
-	fmt.Println("Platform:       ", platform)
-	fmt.Println("GoVersion:      ", goVersion)
+	fmt.Println("Version:        ", Version)
+	fmt.Println("BuildDate:      ", BuildDate)
+	fmt.Println("Platform:       ", Platform)
+	fmt.Println("GoVersion:      ", GoVersion)
 }
